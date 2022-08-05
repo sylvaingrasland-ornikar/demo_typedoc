7d37a297d9885fac94d3853c4787bc2c67b92c8e
sylvaingrasland-ornikar <sylvain.grasland@ornikar.com>
2022-08-05 16:03:17 +0200

    second commit

--- src/example.ts
+++ src/example.ts
@@ -29,6 +29,15 @@ export class Greeter extends Messager {
     greet(additionnalInfo: string) {
       return "Hello, " + this.greeting + " " + additionnalInfo;
     }
+
+    /**
+     * Test protected
+     * @param info info
+     * @returns the info
+     */
+    protected testProtected(info: string) {
+      return info;
+    }
 }
 
 /**
@@ -53,4 +62,34 @@ export enum ExampleEnum {
 export type Person = {
     name: string;
     age: number;
+}
+
+/**
+ * Compute the sum of 2 args
+ * ```ts
+ * // Example of use
+ * const a = 3;
+ * const b = 4;
+ * const c = demoSumWithMarkdown(a, b); 
+ * ```
+ * | Column1 | Column2 |
+ * | ------- | ------- |
+ * | Value1.2 | Value1.2 |
+ * | Value 2.1 | Value2.2 |
+ * [link to NestJS documentation](https://docs.nestjs.com/)
+ * @param a First arg
+ * @param b Second arg
+ * @returns Sum of 2 args
+ */
+export function demoSumWithMarkdown(a: number, b: number) {
+  return a+b
+}
+
+/**
+ * @deprecated
+ * @param input input string
+ * @returns concatenation of input string and NOPE
+ */
+export function demoDeprecated(input: string) {
+  return input + ' NOPE';
 }
